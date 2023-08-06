43735ef39ebd34bf81ebe3647be0ead4e52e75fd
bohm <vagnerpelais@gmail.com>
2023-08-06 15:26:29 -0300

    added post character

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -10,11 +10,25 @@ namespace net_rpg.Controllers
     [Route("api/[controller]")]
     public class CharacterController : ControllerBase
     {
-        private static Character Knight = new Character();
+        private static List<Character> characters = new List<Character> {
+            new Character(),
+            new Character {Id = 1, Name = "Frodo"}
+        };
 
-        [HttpGet]
-        public ActionResult<Character> Get() {
-            return Ok(Knight);
+        [HttpGet("GetAll")]
+        public ActionResult<List<Character>> Get() {
+            return Ok(characters);
+        }
+
+        [HttpGet("GetSingle/{id}")]
+        public ActionResult<Character> GetSingle(int id) {
+            return Ok(characters.FirstOrDefault(c => c.Id == id));
+        }
+
+        [HttpPost("AddCharacter")]
+        public ActionResult<List<Character>> AddCharacter(Character newCharacter) {
+            characters.Add(newCharacter);
+            return Ok(characters);
         }
     }
 }
