bab553019f09d8a90d5cd1d53749b363af3c1f5a
bohm <vagnerpelais@gmail.com>
2023-08-07 09:55:55 -0300

    added database configurations

--- Program.cs
+++ Program.cs
@@ -1,11 +1,14 @@
 global using net_rpg.Models;
 global using net_rpg.Dtos.Character;
+global using Microsoft.EntityFrameworkCore;
+global using net_rpg.Data;
 using net_rpg.Services.CharacterService;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<DataContext>(options =>
+options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
